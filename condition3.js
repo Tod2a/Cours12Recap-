29810150b81f3768cdbac489fea6bdcac2eed998
Thomas <t.copin@yahoo.fr>
2022-12-12 18:40:16 +0100

    condition3validé

---
+++ condition3.js
@@ -0,0 +1,7 @@
+let age = prompt("Quel est votre age?");
+let ageMin = prompt("Quel est l'age minimum?");
+if(age<ageMin){
+    console.log("Age requis non atteint")
+} else {
+    console.log("age requis atteint, vous pouvez entrer :)")
+}
